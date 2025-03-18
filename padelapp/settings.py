31083be28fee33825f4f3500ad16a13dd02ad92b
dario <hdrdisweb@gmail.com>
2025-03-18 09:48:56 +0100

    Corrección de configuración de archivos estáticos en settings.py

--- padelapp/settings.py
+++ padelapp/settings.py
@@ -116,7 +116,7 @@ STATICFILES_DIRS = [
 
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")  # Para la recolección de archivos estáticos
 
-# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
